8b5737a1ad882da07d81b17db2850d6942f426ee
Ayu QA <ayuhotmaidanaiborhu@gmail.com>
2025-06-16 21:21:08 +0700

    Perubahan Judul TC

--- cypress/e2e/Quiz3/login.cy.js
+++ cypress/e2e/Quiz3/login.cy.js
@@ -1,10 +1,11 @@
-describe ('TC 1 -> Check login page', () => {
+describe ('TC 1 (Positive) -> Check login page', () => {
     it('should be succes show login page correctly', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
         // cy.get(".orangehrm-login-logo") //.should('be.visible'); 
         cy.get(".oxd-sheet.oxd-sheet--rounded.oxd-sheet--gutters.oxd-sheet--gray-lighten-2.orangehrm-demo-credentials")
             .should('be.visible').and('contain.text','Username : Admin').and('contain.text','Password : admin123');
-        cy.get("input[placeholder='Username']").should('exist').and('be.visible').and('have.value', '');
+        cy.wait(10000);
+        cy.xpath("//input[@placeholder='Username']").should('exist').and('be.visible').and('have.value', '');
         cy.get("input[placeholder='Password']").should('exist').and('be.visible').and('have.value', '');
         cy.get("button[type='submit']").should('be.enabled').and('be.visible');
         cy.get(".oxd-text.oxd-text--p.orangehrm-login-forgot-header").should('be.visible');
@@ -13,11 +14,11 @@ describe ('TC 1 -> Check login page', () => {
 
 });
 
-describe ('TC 2 -> Click button Login without input User Name & Password', () => {
+describe ('TC 2 (Negative) -> Click button Login without input User Name & Password', () => {
     it('cant be login when Credential NULL (not inputed yet)', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-        cy.wait(1000);
-        cy.xpath("//input[@placeholder='Username']").should('be.visible');
+        cy.wait(10000);
+        cy.get("input[placeholder='Username']").should('be.visible');
         cy.get("input[placeholder='Password']").should('be.visible');
         cy.get("button[type='submit']").click();
         cy.xpath("//div[@class='orangehrm-login-slot-wrapper']//div[1]//div[1]//span[1]").should('be.visible').and('contain.text', 'Required');
@@ -26,7 +27,7 @@ describe ('TC 2 -> Click button Login without input User Name & Password', () =>
 });
 
 
-describe ('TC 3 -> Login with valid User Name & Password', () => {
+describe ('TC 3 (Positive) -> Login with valid User Name & Password', () => {
     it('Success login when credential is correct', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
         cy.get("input[placeholder='Username']").type('Admin');
@@ -41,10 +42,10 @@ describe ('TC 3 -> Login with valid User Name & Password', () => {
 });
 
 
-describe ('TC 4 -> Login with invalid User Name & Password', () => {
+describe ('TC 4 (Negative) -> Login with invalid User Name & Password', () => {
     it('cant be login when credential wrong', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-        cy.wait(1000);
+        //cy.wait(1000);
         cy.xpath("//input[@placeholder='Username']").type('Admin');
         cy.get("input[placeholder='Password']").type ('Admin123');
         cy.get("button[type='submit']").click();
@@ -55,7 +56,7 @@ describe ('TC 4 -> Login with invalid User Name & Password', () => {
 
 });
 
-describe ('TC 5 -> Click textlink forgot your password', () => {
+describe ('TC 5 (Positive) -> Click textlink forgot your password', () => {
     it('Show Forgot Password Page', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
         cy.xpath("//input[@placeholder='Username']").type('Admin');
@@ -72,10 +73,9 @@ describe ('TC 5 -> Click textlink forgot your password', () => {
 
 });
 
-describe ('TC 6 -> Check Forgot Pssword Page', () => {
-    it.only('Show Forgot Password Page', () => {
+describe ('TC 6 (Positive) -> Check Forgot Pssword Page', () => {
+    it('Show Forgot Password Page', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-        cy.wait(1000);
         cy.xpath("//input[@placeholder='Username']").type('Admin');
         cy.get("input[placeholder='Password']").type ('Admin123');
         cy.get("button[type='submit']").click();
